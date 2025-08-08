d89b787e0aa56d6adce591240bacd2d3c3910ed9
Rocy-June <1048654629@qq.com>
2025-08-08 16:32:46 +0900

    重构分析逻辑

---
+++ StarResonanceDpsAnalysis/Core/PacketData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarResonanceDpsAnalysis.Core
+{
+    public class PacketData(object packet)
+    {
+        public object Packet { get; set; } = packet;
+    }
+}
