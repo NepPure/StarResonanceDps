8fc02de79629d02b24095d7669e7c4f955397fad
ihonliu <ihonliu@gmail.com>
2025-09-09 18:54:11 +0800

    chore: 增加职业资料词典

---
+++ StarResonanceDpsAnalysis.WPF/Models/Classes.cs
@@ -0,0 +1,52 @@
+﻿namespace StarResonanceDpsAnalysis.WPF.Models;
+
+/// <summary>
+///     职业
+/// </summary>
+public enum Classes
+{
+    /// <summary>
+    ///     未知
+    /// </summary>
+    Unknown = 0,
+
+    /// <summary>
+    ///     神盾骑士
+    /// </summary>
+    ShieldKnight,
+
+    /// <summary>
+    ///     巨刃守卫者
+    /// </summary>
+    HeavyGuardian,
+
+    /// <summary>
+    ///     雷影剑士
+    /// </summary>
+    Stormblade,
+
+    /// <summary>
+    ///     青岚骑士
+    /// </summary>
+    WindKnight,
+
+    /// <summary>
+    ///     冰魔导师
+    /// </summary>
+    FrostMage,
+
+    /// <summary>
+    ///     神射手
+    /// </summary>
+    Marksman,
+
+    /// <summary>
+    ///     森语者
+    /// </summary>
+    VerdantOracle,
+
+    /// <summary>
+    ///     灵魂乐手
+    /// </summary>
+    SoulMusician
+}
