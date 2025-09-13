d6337fc4afd4845808e1c3c4f52ea84085fcbb00
Rocy-June <1048654629@qq.com>
2025-09-13 15:36:42 +0900

    合并多个类型的数字简写方式

--- StarResonanceDpsAnalysis.Core/Extends/System/Int64Extends.cs
+++ StarResonanceDpsAnalysis.Core/Extends/System/Int64Extends.cs
@@ -9,57 +9,10 @@ namespace StarResonanceDpsAnalysis.Core.Extends.System
 {
     public static class Int64Extends
     {
-        private static string[] CompactSuffixes { get; } = [string.Empty, "K", "M", "B", "T", "Q"];
-        private static string[] ChineseUnits { get; } = [string.Empty, "万", "亿", "兆", "吉"];
-
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static long ShiftRight16(this long value)
         {
             return value >> 16;
-        }
-
-        /// <summary>
-        /// 转换为英文简写 (K / M / B / T ...)
-        /// </summary>
-        /// <param name="number">原始数值</param>
-        /// <param name="digits">保留小数位数</param>
-        /// <returns>如 1200 -> "1.2K"</returns>
-        public static string ToCompactString(this long number, int digits = 2)
-        {
-            if (number < 1000) return number.ToString($"F{digits}");
-
-            var index = 0;
-            double value = number;
-
-            while (value >= 1000 && index < CompactSuffixes.Length - 1)
-            {
-                value /= 1000d;
-                index++;
-            }
-
-            return $"{value.ToString($"F{digits}")}{CompactSuffixes[index]}";
-        }
-
-        /// <summary>
-        /// 转换为中文数字单位 (万 / 亿 / 万亿 / 千万亿 ...)
-        /// </summary>
-        /// <param name="number">原始数值</param>
-        /// <param name="digits">保留小数位数</param>
-        /// <returns>如 120000 -> "12万"</returns>
-        public static string ToChineseUnitString(this long number, int digits = 2)
-        {
-            if (number < 10000) return number.ToString($"F{digits}");
-
-            var index = 0;
-            double value = number;
-
-            while (value >= 10000 && index < ChineseUnits.Length - 1)
-            {
-                value /= 10000d;
-                index++;
-            }
-
-            return value.ToString($"F{digits}") + ChineseUnits[index];
         }
     }
 }
